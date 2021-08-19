cba4bef07304c65ad94e41476084582319db305c
jinwoo <bluekjw3516@naver.com>
2021-08-20 04:06:43 +0900

    for문 및 split, replace 메서드

---
+++ 웹프로그래밍/FE/for문.js
@@ -0,0 +1,16 @@
+var arr = [1,2,3];
+var len = arr.length;
+for(var i = 0; i < len; i++) {
+	
+}
+
+//forEach
+
+var result = "ab:cd".replace(":", "$");
+console.log(result);
+
+var jin = "ab:cd".split(":");
+console.log(jin);
+
+var k = "ab:cd".split("");
+console.log(k);
