304798a37cceabbca171e73e3f1e59617a3256ab
jinwoo <bluekjw3516@naver.com>
2021-08-22 12:20:26 +0900

    애너그램 구현 코드

---
+++ 알고리즘/anagrams.py
@@ -0,0 +1,8 @@
+def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    anagrams = collections.defaultdict(list)
+
+
+    for word in strs:
+        # 정렬하여 딕셔너리에 추가
+        anagrams[''.join(sorted(word))].append(word)
+    return anagrams.values()
