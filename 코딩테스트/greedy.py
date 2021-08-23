a442782f22b22bb081dd307fe9955eff50ff7565
jinwoo <bluekjw3516@naver.com>
2021-08-23 19:18:51 +0900

    그리디 알고리즘

---
+++ 코딩테스트/greedy.py
@@ -0,0 +1,56 @@
+# 그리디 알고리즘 = 현재 상황에서 지금 당장 좋은 것만 고르는 방법
+
+# 1이 될 때까지 최적의 방법으로 계산하기
+
+n, k = map(int, input().split())
+
+result = 0
+
+while True:
+    # N이 K로 나누어 떨어지는 수가 될 때까지 빼기
+    target = (n // k) * k
+    result += (n - target)
+    n = target
+    # N이 K보다 작을 때 (더 이상 나눌 수 없을 때) 반복문 탈출
+    if n < k:
+        break
+    # K로 나누기
+    result += 1
+    n //= k
+
+# 마지막으로 남은 수에 대하여 1씩 빼기
+result += (n - 1)
+print(result)
+
+# 파이썬에서는 수에 대한 제한이 없다.
+
+data = input()
+
+# 첫 번째 문자를 숫자로 변경하여 대입
+result = int(data[0])
+
+for i in range(1, len(data)):
+    # 두 수 중에서 하나라도 '0' 혹은 '1'인 경우, 곱하기보다는 더하기 수행
+    num = int(data[i])
+    if num <= 1 or result <= 1:
+        result += num
+    else:
+        result *= num
+
+print(result)
+
+# 모험가 길드 문제(공포도, 그룹 수의 최댓값 구하기)
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
+
+result = 0 # 총 그룹의 수
+count = 0 # 현재 그룹에 포함된 모험가의 수
+
+for i in data:
+    count += 1
+    if count >= i:
+        result += 1
+        count = 0
+
+print(result)
