a0b5156821329b24f959273ea6af4524ecbb7f75
jinwoo <bluekjw3516@naver.com>
2021-08-23 18:50:55 +0900

    두 수의 합으로 타겟을 만들기

---
+++ 알고리즘/array.py
@@ -0,0 +1,15 @@
+# 두 수의 합으로 타겟을 만들 수 있는 숫자 인덱스를 리턴
+# 브루트 포스로 계산
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+
+def twoSum2(self, nums: List[int], target: int) -> List[int]:
+    for i, n in enumerate(nums):
+        complement = target - n
+
+        if complement in nums[i + 1:]:
+            return nums.index(n), nums[i + 1:].index(complement) + (i + 1)
+
