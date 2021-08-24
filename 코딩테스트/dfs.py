8c340f06c710b115b6daff94fc886872c1cedad3
jinwoo <bluekjw3516@naver.com>
2021-08-24 16:50:32 +0900

    DFS 구조

---
+++ 코딩테스트/dfs.py
@@ -0,0 +1,72 @@
+# # 재귀 함수
+# def recursive_function():
+#     print('재귀 함수를 호출합니다.')
+#     recursive_function()
+#
+# recursive_function()
+
+# 재귀 함수, 종료 조건을 명시
+def recursive_function(i):
+    # 100번째 호출을 했을 때 종료되도록 종료 조건 명시
+    if i == 100:
+        return
+    print(i, '번째 재귀함수에서', i + 1, '번째 재귀 함수를 호출합니다.')
+    recursive_function(i + 1)
+    print(i, '번째 재귀 함수를 호출합니다.')
+
+recursive_function(1)
+
+# 팩토리얼 구현
+# 반복적으로 구현한 n!
+def factorial_iterative(n):
+    result = 1
+    # 1부터 n까지의 수를 차례대로 곱하기
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+# 재귀적으로 구현한 n!
+def factorial_recursive(n):
+    if n <= 1: # n이 1이하인 경우 1을 반환
+        return 1
+    # n! = n * (n - 1)!를 그대로 코드로 작성하기
+    return n * factorial_recursive(n - 1)
+
+print('반복적으로 구현:', factorial_iterative(5))
+print('재귀적으로 구현:', factorial_recursive(5))
+
+# 유클리드 호제법
+def gcd(a, b):
+    if a % b == 0:
+        return b
+    else:
+        return gcd(b, a % b)
+
+print(gcd(192, 162))
+
+# DFS
+def dfs(graph, v, visited):
+    # 현재 노드를 방문 처리
+    visited[v] = True
+    print(v, end=' ')
+    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+visited = [False] * 9
+
+dfs(graph, 1, visited)
+
