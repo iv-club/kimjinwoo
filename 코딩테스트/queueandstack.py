719813208cff990abd5ffb870bcec51835fdd7a2
jinwoo <bluekjw3516@naver.com>
2021-08-24 16:34:36 +0900

    큐와 스택 구조

---
+++ 코딩테스트/queueandstack.py
@@ -0,0 +1,33 @@
+# 스택 구조 : 먼저 들어 온 데이터가 나중에 나가는 형식
+stack = []
+
+stack.append(5)
+stack.append(2)
+stack.append(3)
+stack.append(7)
+stack.pop()
+stack.append(1)
+stack.append(4)
+stack.pop()
+
+print(stack[::-1]) # 최상단 원소부터 출력
+print(stack) # 최하단 원소부터 출력
+
+# 큐 자료구조 : 먼저 들어 온 데이터가 먼저 나가는 형식
+from collections import deque
+
+# 큐(Queue) 구현을 위해 deque 라이브러리 사용
+queue = deque()
+
+queue.append(5)
+queue.append(2)
+queue.append(3)
+queue.append(7)
+queue.popleft()
+queue.append(1)
+queue.append(4)
+queue.popleft()
+
+print(queue) # 먼저 들어온 순서대로 출력
+queue.reverse()
+print(queue) # 나중에 들어온 원소부터 출력
