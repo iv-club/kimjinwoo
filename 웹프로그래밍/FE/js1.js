808ba83d74915bc18127661dc33d384a28a8c7e8
jinwoo <bluekjw3516@naver.com>
2021-09-24 16:45:39 +0900

    자바스크립트 기초

---
+++ 웹프로그래밍/FE/js1.js
@@ -0,0 +1,11 @@
+let name = "Mike";
+age = 30;
+
+alert(name);
+console.log(age);
+
+const name2 = prompt("이름을 입력하세요.");
+alert("환영합니다, " + name + "님");
+
+const name3 = prompt("이름을 입력하세요.");
+alert(`안녕하세요, ${name}님. 환영합니다.`);
