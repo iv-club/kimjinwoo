259eeb7fc8a46063125435dc9aeb5ab62bce3b6a
jinwoo <bluekjw3516@naver.com>
2021-09-26 23:46:34 +0900

    spring과 react 연동

---
+++ project/React/customAxios.js
@@ -0,0 +1,14 @@
+import axios from 'axios'; // 액시오스
+
+export default function customAxios(url, callback) {
+  axios(
+    {
+      url: '/api' + url,
+      method: 'post',
+      baseURL: 'http://localhost:8080',
+      withCredentials: true, 
+    }
+  ).then(function (response) {
+    callback(response.data);
+  });
+}
